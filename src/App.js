7c54d185f0b28739358c6df8f2dcc0f894d51e4c
Kevin Jin <kevjin2002@gmail.com>
2022-02-27 15:24:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const answerArray = ['Ask a question, then click me!', 'It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no'];
+
+  const [display, setDisplay] = useState(0);
+
+  const changeState = (() => {
+  });
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className="centered">
+        <div className="outer-ball centered">
+
+          <button className="inner-ball centered" onClick={changeState}>
+            {answerArray[display]}
+          </button>
+        </div>
+      </section>
   );
 }
 
