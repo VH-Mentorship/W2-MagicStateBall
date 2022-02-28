ff83c2c0ade95e50f1ced50896247f0161e444bb
Kevin Jin <kevjin2002@gmail.com>
2022-02-27 18:14:11 -0600

    Updated comments

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
 
   const answerArray = ['Ask a question, then click me!', 'It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no'];
 
-  const [display, setDisplay] = useState(0);
+  const [displayIndex, setDisplayIndex] = useState(0); // Initialize state with 0
 
   const changeState = (() => {
+        // WRITE CODE HERE
   });
 
 
@@ -17,7 +18,7 @@ function App() {
         <div className="outer-ball centered">
 
           <button className="inner-ball centered" onClick={changeState}>
-            {answerArray[display]}
+            {answerArray[displayIndex]} {/* Access the value specified by displayIndex */}
           </button>
         </div>
       </section>
