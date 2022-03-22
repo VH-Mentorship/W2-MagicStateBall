70c3389c200333346fb82088a326565893305849
Kevin Jin <kevjin2002@gmail.com>
2022-03-22 14:53:27 -0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  const answerArray = ['Ask a question, then click me!', 'It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no'];
+  const answerArray = [
+    "Ask a question, then click me!",
+    "It is certain",
+    "Without a doubt",
+    "You may rely on it",
+    "Yes definitely",
+    "It is decidedly so",
+    "As I see it, yes",
+    "Most likely",
+    "Yes",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Better not tell you now",
+    "Ask again later",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don’t count on it",
+    "Outlook not so good",
+    "My sources say no",
+    "Very doubtful",
+    "My reply is no",
+  ];
 
   const [displayIndex, setDisplayIndex] = useState(0); // Initialize state with 0
 
-  const changeState = (() => {
-        setDisplayIndex(Math.floor(Math.random() * (answerArray.length-1))+1)
-  });
-
-
   return (
-      <section className="centered">
-        <div className="outer-ball centered">
-
-          <button className="inner-ball centered" onClick={changeState}>
-            {answerArray[displayIndex]} {/* Access the value specified by displayIndex */}
-          </button>
-        </div>
-      </section>
+    <section className="centered">
+      <div className="outer-ball centered">
+        <button className="inner-ball centered">
+          {answerArray[displayIndex]}
+          {/* Access the value specified by displayIndex */}
+        </button>
+      </div>
+    </section>
   );
 }
 
